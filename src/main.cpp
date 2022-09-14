c8b6444763befc05788dbaa4c6b60f126357c063
myers3jm <myers3jm@mail.uc.edu>
2022-09-14 10:21:20 -0400

    game: Updating CMake call to find SDL2 to only happen on Unix systems

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <SDL.h>
 
 #include "Game.h"
 
-
+#ifdef _WIN32
+int SDL_main(int argc, char** argv)
+#else
 int main(int argc, char** argv)
+#endif
 {
     Game game;
     bool success = game.Initialize();
